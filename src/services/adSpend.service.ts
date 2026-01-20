ab3dcea05acad670fffc3378f86a3cc7083d85ce
Whyte Peter <peteremmanuelwhyte@gmail.com>
2026-01-20 16:23:53 +0100

    fixed build error

--- src/services/adSpend.service.ts
+++ src/services/adSpend.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { ChartData } from '../types';
+import type { ChartData } from '@/types/chart.types';
 
 /**
  * Simulates API call to fetch ad spend data
